75a2c3c1e3b32270b2ac7466f10816dbb007a092
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-05-19 11:42:56 +0100

    Add Generic Collections

---
+++ StringCollections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] {"A","Y","V","E","A"};
+
+            List<Program> list=array;
+            Stack<Program> stack=array;
+            Queue<Program> queue=array;
+            HashSet<Program> hashset=array;
+
+            foreach (string l in list)
+            {
+                Console.WriteLine($"{l}");
+            }
+            foreach (string s in stack)
+            {
+                Console.WriteLine($"{s}");
+            }
+            foreach (string q in queue)
+            {
+                Console.WriteLine($"{q}");
+            }
+            foreach (string h in hashset)
+            {
+                Console.WriteLine($"{h}");
+            }
+            
+                
+            
+
+        }
+    }
+}
