fee0f451605dd6000d6e3aea868cc1cc785601c3
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-05-19 12:05:29 +0100

    Add Print Collections

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -9,23 +9,38 @@ namespace StringCollections
         {
             string[] array = new string[] {"A","Y","V","E","A"};
 
-            List<Program> list=array;
-            Stack<Program> stack=array;
-            Queue<Program> queue=array;
-            HashSet<Program> hashset=array;
-
+            List<string> list=new List<string>();
+            list.AddRange(array);
+            Stack<string> stack= new Stack<string>();
+            stack.Push("A");
+            stack.Push("Y");
+            stack.Push("V");
+            stack.Push("E");
+            stack.Push("A");
+            Queue<string> queue= new Queue<string>();
+            queue.Enqueue("A");
+            queue.Enqueue("Y");
+            queue.Enqueue("V");
+            queue.Enqueue("E");
+            queue.Enqueue("A");
+            HashSet<string> hashset= new HashSet<string>(array);
+            
+            Console.WriteLine($"List");
             foreach (string l in list)
             {
                 Console.WriteLine($"{l}");
             }
+            Console.WriteLine($"stack");
             foreach (string s in stack)
             {
                 Console.WriteLine($"{s}");
             }
+            Console.WriteLine($"queue");
             foreach (string q in queue)
             {
                 Console.WriteLine($"{q}");
             }
+            Console.WriteLine($"hashset");
             foreach (string h in hashset)
             {
                 Console.WriteLine($"{h}");
